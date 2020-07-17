8ba6e7b2de5d2d5acbf8b00ab762284db3bb9204
AnahiCb <ecabrerab1@est.ups.edu.ec>
2020-07-17 11:02:52 -0500

    Creación de los atributos y métodos de la clase usuario

--- src/ec/edu/ups/modelo/Usuario.java
+++ src/ec/edu/ups/modelo/Usuario.java
@@ -5,10 +5,97 @@
  */
 package ec.edu.ups.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author Anahi
  */
 public class Usuario {
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String contraseña;
+
+    public Usuario() {
+    }
+    
+    public Usuario(String cedula, String nombre, String apellido, String correo, String contraseña) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.contraseña = contraseña;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    
+    public String validarEspacios(String cadena, int espacios) {
+        if (cadena.length() == espacios) {
+            return cadena;
+        } else {
+            if (cadena.length() < espacios) {
+                cadena = llenarEspacios(cadena, espacios);
+                return cadena;
+            } else {
+                cadena = cortarEspacios(cadena, espacios);
+                return cadena;
+            }
+        }
+
+    }
+
+    public String llenarEspacios(String cadena, int espacios) {
+        return String.format("%-" + espacios + "s", cadena);
+    }
+
+    public String cortarEspacios(String cadena, int espacios) {
+        return cadena.substring(0, espacios);
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "cedula=" + cedula + ", nombre=" + nombre + ", apellido=" + apellido + ", correo=" + correo + ", contrase\u00f1a=" + contraseña + '}';
+    }
+    
     
 }
