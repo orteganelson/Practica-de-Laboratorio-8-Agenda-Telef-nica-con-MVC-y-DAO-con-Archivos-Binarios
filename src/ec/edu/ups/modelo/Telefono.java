2634147c371e5acc46501bb4d473a7fe9421e6ff
AnahiCb <ecabrerab1@est.ups.edu.ec>
2020-07-17 12:54:53 -0500

    Creación de los atributos y métodos de la clase telefono

--- src/ec/edu/ups/modelo/Telefono.java
+++ src/ec/edu/ups/modelo/Telefono.java
@@ -10,5 +10,113 @@ package ec.edu.ups.modelo;
  * @author Anahi
  */
 public class Telefono {
+    private int codigo;
+    private String numero;
+    private String tipo;
+    private String operadora;
+    private Usuario usuario;
+
+    public Telefono() {
+    }
+
+    public Telefono(int codigo, String numero, String tipo, String operadora) {
+        this.codigo = codigo;
+        this.numero = numero;
+        this.tipo = tipo;
+        this.operadora = operadora;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getOperadora() {
+        return operadora;
+    }
+
+    public void setOperadora(String operadora) {
+        this.operadora = operadora;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public String validarEspacios(String cadena, int espacios) {
+        if (cadena.length() == espacios) {
+            return cadena;
+        } else {
+            if (cadena.length() < espacios) {
+                cadena = llenarEspacios(cadena, espacios);
+                return cadena;
+            } else {
+                cadena = cortarEspacios(cadena, espacios);
+                return cadena;
+            }
+        }
+
+    }
+
+    public String llenarEspacios(String cadena, int espacios) {
+        return String.format("%-" + espacios + "s", cadena);
+    }
+
+    public String cortarEspacios(String cadena, int espacios) {
+        return cadena.substring(0, espacios);
+    }
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 31 * hash + this.codigo;
+        return hash;
+    }
+    
+     @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Telefono other = (Telefono) obj;
+        if (this.codigo != other.codigo) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Telefono{" + "codigo=" + codigo + ", numero=" + numero + ", tipo=" + tipo + ", operadora=" + operadora + '}';
+    }
+    
     
 }
