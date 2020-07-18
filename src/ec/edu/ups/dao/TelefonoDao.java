21137f5458fdb235734fa7a9062a4b21c1aea3cd
AnahiCb <ecabrerab1@est.ups.edu.ec>
2020-07-18 14:25:49 -0500

    Creación de los atributos y métodos de las clases del DAO

--- src/ec/edu/ups/dao/TelefonoDao.java
+++ src/ec/edu/ups/dao/TelefonoDao.java
@@ -5,10 +5,199 @@
  */
 package ec.edu.ups.dao;
 
+import ec.edu.ups.idao.ITelefonoDao;
+import ec.edu.ups.modelo.Telefono;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.HashMap;
+import java.util.*;
+import java.util.Map;
+
 /**
  *
  * @author Anahi
  */
-public class TelefonoDao {
+public class TelefonoDao implements ITelefonoDao {
+
+    private int codigo;
+    private int tamañoRegistro;
+    private RandomAccessFile archivo;
+
+    public TelefonoDao() {
+        codigo = 0;
+        tamañoRegistro = 92;
+        try {
+            
+            archivo = new RandomAccessFile("datos/telefono.dat", "rw");
+        } catch (IOException ex) {
+            System.out.println("error de escritura y lectura(teelfonoDAO)");
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void create(Telefono telefono) {
+        try {
+            codigo++;
+            archivo.seek(archivo.length());
+            archivo.writeInt(telefono.getCodigo());
+            archivo.writeUTF(telefono.getNumero());
+            archivo.writeUTF(telefono.getOperadora());
+            archivo.writeUTF(telefono.getTipo());
+            archivo.writeUTF(telefono.getUsuario().getCedula());
+        } catch (IOException ex) {
+            System.out.println("Error (create Telefono)");
+        }
+
+    }
+
+    @Override
+    public Telefono read(int id) {
+        try {
+            int salto = 0;
+            while (salto < archivo.length()) {
+                archivo.seek(salto);
+                int codigoArchivo = archivo.readInt();
+                if (id == codigoArchivo) {
+                    Telefono tele = new Telefono(codigoArchivo, archivo.readUTF(), archivo.readUTF(),
+                            archivo.readUTF());
+                    return tele;
+                }
+                salto += tamañoRegistro;
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Error read telefono");
+        }
+        return null;
+    }
+
+    @Override
+    public void update(Telefono telefono) {
+
+        int salto = 0;
+        int codigo = telefono.getCodigo();
+        try {
+            while (salto < archivo.length()) {
+                archivo.seek(salto);
+                int codigoArchivo = archivo.readInt();
+                if (codigo == codigoArchivo) {
+                    archivo.writeUTF(telefono.getNumero());
+                    archivo.writeUTF(telefono.getOperadora());
+                    archivo.writeUTF(telefono.getTipo());
+                    break;
+                }
+                salto += tamañoRegistro;
+            }
+        } catch (IOException ex) {
+            System.out.println("Error de lectura o escritura(upDate Telefono)");
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+
+        int salto = 0;
+
+        try {
+            while (salto < archivo.length()) {
+                archivo.seek(salto);
+                int codigoArchivo = archivo.readInt();
+                if (id == codigoArchivo) {
+                    archivo.seek(salto);
+                    archivo.writeInt(0);
+                    archivo.writeUTF(llenarEspacios(20));
+                    archivo.writeUTF(llenarEspacios(25));
+                    archivo.writeUTF(llenarEspacios(25));
+                    archivo.writeUTF(llenarEspacios(10));
+                    break;
+                }
+                salto += tamañoRegistro;
+            }
+        } catch (IOException ex) {
+            System.out.println("Error de lectura o escritura(upDate Telefono)");
+        }
+
+    }
+
+    @Override
+    public String llenarEspacios(int espacios) {
+        String formato = "";
+        return String.format("%-" + espacios + "s", formato);
+    }
+
+    @Override
+    public List<Telefono> findAll() {
+        List<Telefono> listaTelefonos = new ArrayList<>();
+        try {
+            int salto = 0;
+
+            while (salto < archivo.length()) {
+                archivo.seek(salto);
+
+                int valor = archivo.readInt();
+                if (valor > 0) {
+                    Telefono tele = new Telefono(valor, archivo.readUTF().trim(),
+                            archivo.readUTF().trim(), archivo.readUTF().trim());
+                    listaTelefonos.add(tele);
+                }
+
+                salto += tamañoRegistro;
+            }
+            return listaTelefonos;
+        } catch (IOException ex) {
+            System.out.println("error find all telefono");
+        }
+        return listaTelefonos;
+    }
+
+    @Override
+    public List<Telefono> telefonosUsuario(String id) {
+        List<Telefono> teles = new ArrayList<>();
+
+        try {
+            int salto = 80;
+            while (salto < archivo.length()) {
+                archivo.seek(salto);
+                String aux = archivo.readUTF().trim();
+                System.out.println(aux);
+                if (aux.equals(id.trim())) {
+
+                    archivo.seek(salto - 80);
+                    int valor = archivo.readInt();
+                    if (valor > 0) {
+                        Telefono tele = new Telefono(valor, archivo.readUTF().trim(),
+                                archivo.readUTF().trim(), archivo.readUTF().trim());
+                        teles.add(tele);
+                    }
+
+                }
+                salto += tamañoRegistro;
+            }
+            return teles;
+        } catch (IOException ex) {
+            System.out.println("Error telefonos usuario");
+        }
+
+        return teles;
+    }
+
+    @Override
+    public int codigoTelefono() {
+
+        try {
+            if (archivo.length() > 0) {
+                int aux = (int) (archivo.length() / tamañoRegistro);
+                System.out.println(aux);
+                return aux;
+
+            } else {
+                return 0;
+            }
+        } catch (IOException ex) {
+            System.out.println("Error codigo(codigoTelefono)");
+        }
+        return codigo;
+    }
     
 }
--- src/ec/edu/ups/dao/UsuarioDao.java
+++ src/ec/edu/ups/dao/UsuarioDao.java
@@ -5,10 +5,188 @@
  */
 package ec.edu.ups.dao;
 
+import ec.edu.ups.idao.IUsuarioDao;
+import ec.edu.ups.modelo.Usuario;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.HashMap;
+import java.util.Map;
 /**
  *
  * @author Anahi
  */
-public class UsuarioDao {
+public class UsuarioDao implements IUsuarioDao{
+     private RandomAccessFile archivo;
+    private Usuario usuario;
+    private int registro;
+
+    public UsuarioDao() {
+        registro = 128;
+        try {
+            archivo = new RandomAccessFile("datos/usuario.dat", "rw");
+        } catch (IOException ex) {
+            System.out.println("error de escritura y lectura");
+            ex.printStackTrace();
+        }
+    }
     
+    @Override
+    public void create(Usuario cliente) {
+        try {
+            archivo.seek(archivo.length());
+            archivo.writeUTF(cliente.getCedula());
+            archivo.writeUTF(cliente.getNombre());
+            archivo.writeUTF(cliente.getApellido());
+            archivo.writeUTF(cliente.getCorreo());
+            archivo.writeUTF(cliente.getContraseña());
+        } catch (IOException ex) {
+            System.out.println("Error de lectura y escritura(create usuario)");
+            ex.printStackTrace();
+        }
+    }
+    @Override
+    public Usuario read(String cedula) {
+        int salto = 0;
+        try {
+            while (salto < archivo.length()) {
+                archivo.seek(salto);
+                String cedulaArchivo = archivo.readUTF().trim();
+                if (cedula.trim().equals(cedulaArchivo)) {
+                    Usuario usuario1 = new Usuario(cedula, archivo.readUTF().trim(),
+                            archivo.readUTF().trim(), archivo.readUTF().trim(),
+                            archivo.readUTF().trim());
+                    return usuario1;
+                }
+                salto += registro;
+            }
+        } catch (IOException ex) {
+            System.out.println("Error de lectura o escritura(readUsuario)");
+        }
+        return null;
+    }
+
+    @Override
+    public Usuario readCorreo(String correo) {
+        int salto = 66;
+        try {
+            while (salto < archivo.length()) {
+                archivo.seek(salto);
+
+                String correoArchivo = archivo.readUTF();
+                String contraseñaArchivo = archivo.readUTF();
+
+                System.out.println(correoArchivo);
+                System.out.println(contraseñaArchivo);
+
+                System.out.println(correo);
+
+                if (correo.equals(correoArchivo.trim())) {
+
+                    archivo.seek(salto - 66);
+                    usuario = new Usuario(archivo.readUTF().trim(), archivo.readUTF().trim(),
+                            archivo.readUTF().trim(), correoArchivo, contraseñaArchivo);
+                    return usuario;
+                }
+                salto += registro;
+            }
+        } catch (IOException ex) {
+            System.out.println("Error read Correo");
+        }
+        return null;
+    }
+    @Override
+    public void update(Usuario cliente) {
+
+        int salto = 0;
+        String cedula = cliente.getCedula();
+        try {
+            while (salto < archivo.length()) {
+                archivo.seek(salto);
+                String cedulaArchivo = archivo.readUTF().trim();
+                if (cedula.trim().equals(cedulaArchivo)) {
+                    //archivo.writeUTF(cliente.getCedula());
+                    archivo.writeUTF(cliente.getNombre());
+                    archivo.writeUTF(cliente.getApellido());
+                    archivo.writeUTF(cliente.getCorreo());
+                    archivo.writeUTF(cliente.getContraseña());
+                    break;
+                }
+                salto += registro;
+            }
+        } catch (IOException ex) {
+            System.out.println("Error de lectura o escritura(upDateUsuario)");
+        }
+
+    }
+    @Override
+    public void delete(Usuario cliente) {
+        try {
+            String cedula = cliente.getCedula();
+            int salto = 0;
+            while (salto < archivo.length()) {
+                archivo.seek(salto);
+                String cedulaArchivo = archivo.readUTF();
+                if (cedula.trim().equals(cedulaArchivo.trim())) {
+                    archivo.writeUTF(llenarEspacios(10));
+                    archivo.writeUTF(llenarEspacios(25));
+                    archivo.writeUTF(llenarEspacios(25));
+                    archivo.writeUTF(llenarEspacios(50));
+                    archivo.writeUTF(llenarEspacios(8));
+                    break;
+                }
+                salto += registro;
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Error delete usuario");
+        }
+    }
+
+    public String llenarEspacios(int espacios) {
+        String aux = "";
+        return String.format("%-" + espacios + "s", aux);
+    }
+    @Override
+    public Usuario iniciarSesion(String correo, String contraseña) {
+        try {
+            int salto = 66;
+
+            byte[] correoArreglo = new byte[50];
+            byte[] contraseñaArreglo = new byte[8];
+
+            while (salto < archivo.length()) {
+                archivo.seek(salto);
+
+                String correoArchivo = archivo.readUTF();
+                String contraseñaArchivo = archivo.readUTF();
+
+                System.out.println(correoArchivo);
+                System.out.println(contraseñaArchivo);
+
+                System.out.println(correo + " " + contraseña);
+
+                if (correo.equals(correoArchivo.trim()) && contraseña.equals(contraseñaArchivo.trim())) {
+
+                    archivo.seek(salto - 66);
+                    usuario = new Usuario(archivo.readUTF().trim(), archivo.readUTF().trim(),
+                            archivo.readUTF().trim(), correoArchivo, contraseñaArchivo);
+                    return usuario;
+                }
+                salto += registro;
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Error escritu o lectura (iniciar sesion) ");
+        }
+
+        return null;
+    }
+    @Override
+    public Map<String, Usuario> findAll() {
+        return null;
+    }
 }
